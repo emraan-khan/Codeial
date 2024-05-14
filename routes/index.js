e0d57d32162e785d2ba4e2739859f84548ecb9d4
Imran Khan <imrankhan772244@gmail.com>
2024-05-14 18:13:03 +0530

    likes router

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', homeController.home);
 router.use('/users',require('./user'))
 router.use('/posts', require('./posts'))
 router.use('/comments',require('./comment'))
-
+router.use('/likes',require('./likes'))
 router.use('/api', require('./api'))
 
 module.exports = router;
